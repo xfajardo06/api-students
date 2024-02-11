722374046bfeb95f66298856873b8c69e127d2a0
xfajardo06 <x.fajardoc@gmail.com>
2024-02-10 20:41:54 -0500

    fix: db host by default

--- app/db.py
+++ app/db.py
@@ -6,7 +6,14 @@ db = MongoEngine()
 load_dotenv()
 
 def initialize_db(app):
-    app.config['MONGODB_SETTINGS'] = {
-        'host': os.environ.get('MONGODB_URI')
+    if os.environ.get('MONGODB_URI'):
+        app.config['MONGODB_SETTINGS'] = {
+            'host': os.environ.get('MONGODB_URI')
+        }
+    else:
+        app.config['MONGODB_SETTINGS'] = {
+        'db': 'calificaciones',
+        'host': 'localhost',
+        'port': 27017
     }
     db.init_app(app)
